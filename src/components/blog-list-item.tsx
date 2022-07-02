78ae10508a4225ae4501c1f2ee772cda80641690
Shubham Srivastava <shbm09@gmail.com>
2022-07-02 05:23:13 +0200

    remove date time on blog list

--- src/components/blog-list-item.tsx
+++ src/components/blog-list-item.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
-import React from "react"
+import Reac
+t from "react"
 import { jsx, Link as TLink } from "theme-ui"
 import { Box } from "@theme-ui/components"
 import { Link } from "gatsby"
@@ -26,7 +27,7 @@ const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
     <TLink as={Link} to={post.slug} sx={{ fontSize: [1, 2, 3], color: `text` }}>
       {post.title}
     </TLink>
-    <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+    {/* <p sx={{ color: `secondary`, mt: 1, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
       <time>{post.date}</time>
       {post.tags && showTags && (
         <React.Fragment>
@@ -34,7 +35,7 @@ const BlogListItem = ({ post, showTags = true }: BlogListItemProps) => (
           <ItemTags tags={post.tags} />
         </React.Fragment>
       )}
-    </p>
+    </p> */}
   </Box>
 )
 
